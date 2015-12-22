8201a847fb011ea4b1354ff1a07343e52d3810c5
Michael Lawrence <007mike@gmail.com>
2015-12-21 20:47:38 -0500

    changed directory name intro to basics

---
+++ basics/exercise2.rb
@@ -0,0 +1,8 @@
+thousands = 4323 / 1000
+hundreds = 4323 % 1000 /100
+tens = 4323 %100 /10
+ones = 4323 %10
+puts thousands
+puts hundreds
+puts tens
+puts ones
---
+++ basics/exercise3.rb
@@ -0,0 +1,5 @@
+movies = {:bladerunner=>1982,:starwars=>1977,:friday_the_13th=>1980,:enders_game=>2013}
+puts movies[:bladerunner]
+puts movies[:starwars]
+puts movies[:friday_the_13th]
+puts movies[:enders_game]
