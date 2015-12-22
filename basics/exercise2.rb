eea9a36d463418c8c3786f3dae37949233984f6b
Michael Lawrence <007mike@gmail.com>
2015-12-21 20:49:32 -0500

    removed basics folder from repository

--- basics/exercise2.rb
+++
@@ -1,8 +0,0 @@
-thousands = 4323 / 1000
-hundreds = 4323 % 1000 /100
-tens = 4323 %100 /10
-ones = 4323 %10
-puts thousands
-puts hundreds
-puts tens
-puts ones
--- basics/exercise4.rb
+++
@@ -1,5 +0,0 @@
-dates = [1982,1977,1980,2013]
-puts dates[0]
-puts dates[1]
-puts dates[2]
-puts dates[3]
