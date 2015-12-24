4e2db940345d14bc75de487f77ec48401a8b22c3
Michael Lawrence <007mike@gmail.com>
2015-12-23 21:11:14 -0500

    added arrays solutions to repository

---
+++ arrays/check_for_three.rb
@@ -0,0 +1,13 @@
+#check_for_three.rb
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+arr.each do |num|
+  if num == number
+    puts "Yes, #{number} is in the array"
+  end
+end
+
+
+
---
+++ arrays/two_arrays.rb
@@ -0,0 +1,12 @@
+# two_arrays.rb
+
+arr = [1, 5, 7, 9, 11, 22]
+
+arr2 = []
+
+arr.each do |num|
+  arr2.push(num +2)
+end
+
+p arr
+p arr2
